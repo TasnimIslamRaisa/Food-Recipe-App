29604871870097ef22bd2251aead7b5dfa2d516c
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-02-08 21:16:17 +0600

    add

---
+++ lib/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+//created statefilWidgets because our data will be changed
+
+class home extends StatefulWidget {
+  //const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.greenAccent, Colors.green],
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Text(
+              "Hello Welcome to our App",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+                fontSize: 25,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
